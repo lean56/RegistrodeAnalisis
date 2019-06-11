eb4d021a284510ebeac5cbf0c3ccc5efe7273f0a
leonardo emil <theflaco56@hotmail.com>
2019-06-11 16:43:14 -0400

    Creando Entidad Analisis

---
+++ RegistrodeAnalisis/Entidades/Analisis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrodeAnalisis.Entidades
+{
+   public class Analisis
+    {
+        [Key]
+
+        public int AnalisisId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public virtual List<AnalisisDetalle> Detalles { get; set; }
+
+
+        public Analisis()
+        {
+            AnalisisId = 0;
+            UsuarioId = 0;
+            Fecha = DateTime.Now;
+
+            Detalles = new List<AnalisisDetalle>();
+        }
+    }
+}
