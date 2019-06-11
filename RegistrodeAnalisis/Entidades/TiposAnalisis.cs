22472edeb75f2676df12bcda47bbf37086591179
leonardo emil <theflaco56@hotmail.com>
2019-06-11 16:49:02 -0400

    Creando Entidad TiposAnalisis

---
+++ RegistrodeAnalisis/Entidades/TiposAnalisis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrodeAnalisis.Entidades
+{
+   public class TiposAnalisis
+    {
+        [Key]
+        public int TipoId { get; set; }
+        public string Descripcion { get; set; }
+
+        public TiposAnalisis()
+        {
+            TipoId = 0;
+            Descripcion = string.Empty;
+        }
+    }
+}
