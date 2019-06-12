9faa3b6092241debfc419368e14f419aa7858b92
leonardo emil <theflaco56@hotmail.com>
2019-06-12 16:07:03 -0400

    Creando Ventana Registro de Analisis

---
+++ RegistrodeAnalisis/UI/Registro/RegistrodeAnalisis.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace RegistrodeAnalisis.UI.Registro
+{
+    partial class RegistrodeAnalisis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TipoAnalisisButton = new System.Windows.Forms.Button();
+            this.TipoAnalisiscomboBox = new System.Windows.Forms.ComboBox();
+            this.label02 = new System.Windows.Forms.Label();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.IdnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TipoAnalisisButton
+            // 
+            this.TipoAnalisisButton.Location = new System.Drawing.Point(253, 103);
+            this.TipoAnalisisButton.Name = "TipoAnalisisButton";
+            this.TipoAnalisisButton.Size = new System.Drawing.Size(40, 23);
+            this.TipoAnalisisButton.TabIndex = 43;
+            this.TipoAnalisisButton.Text = "+";
+            this.TipoAnalisisButton.UseVisualStyleBackColor = true;
+            // 
+            // TipoAnalisiscomboBox
+            // 
+            this.TipoAnalisiscomboBox.FormattingEnabled = true;
+            this.TipoAnalisiscomboBox.Location = new System.Drawing.Point(126, 105);
+            this.TipoAnalisiscomboBox.Name = "TipoAnalisiscomboBox";
+            this.TipoAnalisiscomboBox.Size = new System.Drawing.Size(121, 21);
+            this.TipoAnalisiscomboBox.TabIndex = 42;
+            // 
+            // label02
+            // 
+            this.label02.AutoSize = true;
+            this.label02.Location = new System.Drawing.Point(39, 108);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(81, 13);
+            this.label02.TabIndex = 41;
+            this.label02.Text = "Tipo de Analisis";
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(234, 9);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(55, 45);
+            this.BuscarButton.TabIndex = 40;
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(126, 63);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(120, 20);
+            this.NombretextBox.TabIndex = 39;
+            // 
+            // IdnumericUpDown
+            // 
+            this.IdnumericUpDown.Location = new System.Drawing.Point(126, 23);
+            this.IdnumericUpDown.Name = "IdnumericUpDown";
+            this.IdnumericUpDown.Size = new System.Drawing.Size(82, 20);
+            this.IdnumericUpDown.TabIndex = 38;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "ID";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.EliminarButton);
+            this.groupBox1.Controls.Add(this.Guardarbutton);
+            this.groupBox1.Controls.Add(this.Nuevobutton);
+            this.groupBox1.Location = new System.Drawing.Point(42, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 88);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.EliminarButton.Location = new System.Drawing.Point(200, 19);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 56);
+            this.EliminarButton.TabIndex = 6;
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Guardarbutton.Location = new System.Drawing.Point(103, 19);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 56);
+            this.Guardarbutton.TabIndex = 0;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Nuevobutton.Location = new System.Drawing.Point(16, 19);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(71, 56);
+            this.Nuevobutton.TabIndex = 5;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // RegistrodeAnalisis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 275);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TipoAnalisisButton);
+            this.Controls.Add(this.TipoAnalisiscomboBox);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.IdnumericUpDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistrodeAnalisis";
+            this.Text = "Registro de Analisis";
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TipoAnalisisButton;
+        private System.Windows.Forms.ComboBox TipoAnalisiscomboBox;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.NumericUpDown IdnumericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+    }
+}
---
+++ RegistrodeAnalisis/UI/Registro/RegistrodeAnalisis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrodeAnalisis.UI.Registro
+{
+    public partial class RegistrodeAnalisis : Form
+    {
+        public RegistrodeAnalisis()
+        {
+            InitializeComponent();
+        }
+    }
+}
